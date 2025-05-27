149f093aba9b029389963eb7c1e0c4fb252f515b
Muhammed Arif <muhammedarif@Muhammeds-Mac-mini.local>
2025-05-27 10:09:22 +0600

    done necessary things on index file

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x7tmnab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		await client.connect();
+
+		app.get("/products/:id", function (req, res, next) {
+			res.json({ msg: "This is CORS-enabled for all origins!" });
+		});
+
+		await client.db("admin").command({ ping: 1 });
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
